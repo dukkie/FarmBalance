0cb8339f845eaac4701cde785d90945a7cad0744
Dukkie <dukkiedukkie@yahoo.co.jp>
2011-12-05 12:10:56 +0900

    initial commit

---
+++ lib/FarmBalance.pm
@@ -0,0 +1,32 @@
+package FarmBalance;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+FarmBalance -
+
+=head1 SYNOPSIS
+
+  use FarmBalance;
+
+=head1 DESCRIPTION
+
+FarmBalance is
+
+=head1 AUTHOR
+
+DUKKIE E<lt>dukkiedukkie@yahoo.co.jpE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
